00b0f1d7d7edc208d04f5043fb83ed53c1b0ad5b
kalbek <access2kaleb@gmail.com>
2023-07-11 12:08:52 +0300

    Create main.rb as entry point of console app.

---
+++ main.rb
@@ -0,0 +1,60 @@
+require_relative 'item'
+
+def run
+  puts 'Welcome to catalog of things!!'
+  loop do
+    display_menu
+    print "\nEnter your choice: "
+    choice = gets.chomp.to_i
+    if [1, 5, 7].include?(choice)
+      book_options(choice)
+    elsif [2, 4, 8].include?(choice)
+      music_options(choice)
+    elsif [3, 6, 9].include?(choice)
+      game_options(choice)
+    elsif choice.zero?
+      puts 'Thank you for using this app!'
+      exit!
+    else
+      puts 'Invalid input!'
+    end
+  end
+end
+
+def book_options(_choice)
+  puts 'running books options'
+end
+
+def music_options(_choice)
+  puts 'running music options'
+end
+
+def game_options(_choice)
+  puts 'running game options'
+end
+
+def exit(choice)
+  case choice
+  when 0
+    puts 'Thank you for using this app!'
+    exit!
+  else
+    puts 'Invalid choice. Please try again.'
+  end
+end
+
+def display_menu
+  puts "\n"
+  puts 'Please make your choice:'
+  puts '1 - List all books' # books
+  puts '2 - List all music albums' # music
+  puts '3 - List of games' # games
+  puts "4 - List all genres (e.g 'Comedy', 'Thriller')" # music
+  puts "5 - List all labels (e.g. 'Gift', 'New')" # books
+  puts "6 - List all authors (e.g. 'Stephen King')" # games
+  puts '7 - Add a book' # books
+  puts '8 - Add a music album' # music
+  puts '9 - Add a game' # games
+  puts '0 - Quit'
+end
+run
