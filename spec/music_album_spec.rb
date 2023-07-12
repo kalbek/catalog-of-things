5421849abd67105028af6d8d9c1360b047fe21f3
Benson Njuguna <njugunab655@gmail.com>
2023-07-12 10:32:47 +0300

    Add tests for music album class

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../music_album'
+require 'date'
+
+RSpec.describe MusicAlbum do
+  let(:album_id) { 1 }
+  let(:album_publish_date) { Date.today - 15 * 365 }
+  let(:album_on_spotify) { true }
+  let(:album) { MusicAlbum.new(album_id, album_publish_date, album_on_spotify) }
+
+  describe '#initialize' do
+    it 'sets the on_spotify attribute' do
+      expect(album.on_spotify).to eq(album_on_spotify)
+    end
+
+    it 'sets the id attribute' do
+      expect(album.id).to eq(album_id)
+    end
+
+    it 'sets the archived attribute to false' do
+      expect(album.archived).to be_falsey
+    end
+
+    it 'sets the publish_date attribute' do
+      expect(album.publish_date).to eq(album_publish_date)
+    end
+  end
+
+  describe '#can_be_archived?' do
+    it 'returns false if the item is not on Spotify' do
+      album.instance_variable_set(:@on_spotify, false)
+      expect(album.on_spotify).to be_falsey
+    end
+  end
+
+  describe '#move_to_archive' do
+    it 'archives the item if it can be archived' do
+      album.move_to_archive
+      expect(album.archived).to be_truthy
+    end
+
+    it 'does not archive the item if it cannot be archived' do
+      album.instance_variable_set(:@on_spotify, false)
+      album.move_to_archive
+      expect(album.archived).to be_falsey
+    end
+  end
+end
