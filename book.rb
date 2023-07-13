29344f415f14afb020acd218e5cbd120d7b3be29
kalbek <access2kaleb@gmail.com>
2023-07-13 08:58:21 +0300

    Define label class properties.

--- book.rb
+++ book.rb
@@ -1,16 +1,16 @@
 require_relative 'item'
 
 class Book < Item
-    attr_accessor :publisher, :cover_state, :publish_date
-    attr_reader :id
-  
-    def initialize(id, publisher, cover_state, publish_date, label)
-      super(id, publish_date, false)
-      @id = id || Random.rand(1..100)
-      @publisher = publisher
-      @cover_state = cover_state
-      @publish_date = publish_date
-      @label = label
-      update_archived_status
-    end 
+  attr_accessor :publisher, :cover_state, :publish_date
+  attr_reader :id
+
+  def initialize(id, publisher, cover_state, publish_date, label)
+    super(id, publish_date, false)
+    @id = id || Random.rand(1..100)
+    @publisher = publisher
+    @cover_state = cover_state
+    @publish_date = publish_date
+    @label = label
+    update_archived_status
+  end
 end
--- label.rb
+++ label.rb
@@ -1,4 +1,12 @@
 require_relative 'item'
 
-class Label 
+class Label
+  attr_reader :title, :color, :items, :id
+
+  def initialize(id, title, color)
+    @id = id || Random.rand(1..100)
+    @title = title
+    @color = color
+    @items = []
+  end
 end
