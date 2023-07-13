dd506297e064b8dbeb8482ae4e24e9a0635bbf68
massa <zoumaniguimassa12@gmail.com>
2023-07-13 13:47:42 +0000

    Fixing test errors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,8 @@
 require_relative '../game'
 
 RSpec.describe Game do
- let(:game) { Game.new(1, true, '2023-07-01', Date.today) }
+  let(:game) { Game.new(1, true, '2023-07-01', Date.today) }
 
- describe '#add_item' do
-   let(:item) { double('item') }
-   let(:author) { double('author') }
-
-   it 'sets the author on the item' do
-     expect(item).to receive(:author=).with(author)
-     game.add_item(item, author)
-   end
- end
- 
   describe '#add_item' do
     let(:item) { double('item') }
     let(:author) { double('author') }
