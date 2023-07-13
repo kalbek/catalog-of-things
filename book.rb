66444bf1f740ff8c4e723b58b6c0be0991358f9d
kalbek <access2kaleb@gmail.com>
2023-07-13 09:02:54 +0300

    Implement add a book method and save books to json file.

--- book.rb
+++ book.rb
@@ -21,4 +21,71 @@ class Book < Item
   def update_archived_status
     @archived = can_be_archived?
   end
+
+  def self.validate_date
+    date_input = gets.chomp
+
+    begin
+      Date.parse(date_input)
+    rescue ArgumentError
+      print 'Invalid date! Enter valid date: '
+      validate_date
+    end
+  end
+
+  def self.book_details
+    book_data = {
+      publisher: '',
+      cover_state: '',
+      publish_date: Date.today,
+      label_title: '',
+      label_color: ''
+    }
+
+    print "Enter the book's publisher: "
+    book_data[:publisher] = gets.chomp
+
+    print "Books' cover state: "
+    book_data[:cover_state] = gets.chomp
+
+    print 'Publish date (YYYY-MM-DD): '
+    book_data[:publish_date] = validate_date
+
+    print "Book label's title: "
+    book_data[:label_title] = gets.chomp
+
+    print "Book label's color: "
+    book_data[:label_color] = gets.chomp
+
+    book_data
+  end
+
+  def self.add_book
+    # load book and label data from json files
+    book_data = book_details
+    load_books
+
+
+    data = update_loaded_data(book_data)
+
+    book = data[:book]
+    label = data[:label]
+    date = data[:publish_date]
+    loaded_books = data[:loaded_books]
+    loaded_labels = data[:loaded_labels]
+
+    add_book_items_to_list(book_data, book, label, date)
+
+    save_books_to_json(loaded_books)
+    label.save_labels_to_file(loaded_labels)
+  end
+
+  def self.add_book_items_to_list(book_data, book, label, _date)
+    # add book to item list (add a book item in items)
+    items = JSON.parse(File.read('items.json'), symbolize_names: true)
+    item = Item.new(items.length + 1, book_data[:publish_date], book.can_be_archived?)
+    item.label = label
+    label.add_item(item)
+    puts 'Book added successfully!'
+  end
 end
