1080e1559548abaf82ad98d0deccf01ace37cb42
kalbek <access2kaleb@gmail.com>
2023-07-13 08:59:46 +0300

    Implement add_item method in book class.

--- label.rb
+++ label.rb
@@ -9,4 +9,43 @@ class Label
     @color = color
     @items = []
   end
+
+  def add_item(item)
+    return if items.include?(item)
+
+    @items << item
+    item.label = self
+    add_items_to_file(item)
+  end
+
+  def to_json(*_args)
+    {
+      'id' => id,
+      'title' => title,
+      'color' => color
+    }.to_json
+  end
+
+  def add_items_to_file(items)
+    loaded_items = load_items_data
+    loaded_items << items
+    item_data = loaded_items.map do |item|
+      {
+        'id' => item.id,
+        'publish_date' => item.publish_date,
+        'archived' => item.archived,
+        'label' => if item.label.is_a?(Hash)
+                     item.label
+                   else
+                     {
+                       'id' => item.label.id,
+                       'title' => item.label.title,
+                       'color' => item.label.color
+                     }
+                   end
+      }
+    end
+    puts item_data
+    File.write('items.json', JSON.generate(item_data))
+  end
 end
